f93be1468f5c5d6e2b65e5aee75916eb71ae700f
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-07-31 09:12:43 +0530

    changes for the validation

---
+++ src/app/user-validation/user-validation.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-validation',
+  templateUrl: './user-validation.component.html',
+  styleUrls: ['./user-validation.component.css']
+})
+export class UserValidationComponent implements OnInit {
+
+  isValid : boolean = true;
+  profileForm = this.fb.group({
+    firstName: ['',[Validators.required,Validators.maxLength(15),Validators.,Validators.email,Validators.pattern('^[a-z][A-Z]')]],
+
+    lastName: ['',Validators.required],
+    address: this.fb.group({
+      street: ['',Validators.required],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  get f() {return this.profileForm.controls};
+
+  get fsubcontrols() {
+
+    return this.profileForm.get('address') as FormGroup
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit()
+  {
+    this.isValid = this.profileForm.valid;
+   
+  }
+
+}
